58b30190a76ddb040f055192ebc6fa6af90ea724
Ritik Roushan <116073992+ritik-coder-101@users.noreply.github.com>
2025-07-29 00:35:00 +0530

    commit

--- ACCIOJOB-BACK/server.js
+++ ACCIOJOB-BACK/server.js
@@ -12,7 +12,7 @@ const app=express();
 const PORT=process.env.PORT || 5000;
 
 const allowedOrigins = [
-  'http://localhost:3000/signup', // frontend
+  'http://localhost:3000', // frontend
   'http://localhost:5000', // backend
   
 ];
