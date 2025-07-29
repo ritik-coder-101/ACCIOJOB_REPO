f99a928632515db9dfb2c5d540f670a7d53076e0
Ritik Roushan <116073992+ritik-coder-101@users.noreply.github.com>
2025-07-29 10:39:51 +0530

    fixed devloyed url

--- ACCIOJOB-BACK/server.js
+++ ACCIOJOB-BACK/server.js
@@ -14,6 +14,7 @@ const PORT=process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000', // frontend
   'http://localhost:5000', // backend
+  'https://acciojob-repo.vercel.app'
   
 ];
 
